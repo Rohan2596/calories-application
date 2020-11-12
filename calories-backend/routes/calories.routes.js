5b7fb6dc9c5aa27de8214df9c95acc1e94ec23d7
Rohan2596 <rohankadam662@gmail.com>
2020-11-12 09:57:48 +0530

    ADDED:Maintain Basic Routes for Meals functionality && routes to server.js

--- calories-backend/routes/calories.routes.js
+++ calories-backend/routes/calories.routes.js
@@ -1,15 +1,27 @@
-const route =require('express').Router();
+const route = require('express').Router();
 
 
- /*
-   @author  :- Rohan Ravindra Kadam 
-   @Purpose :-  User routes for Login ,Registration 
-                ,password updation and many more.
-   
- */ 
+/*
+  @author  :- Rohan Ravindra Kadam 
+  @Purpose :-  User routes for Login ,Registration 
+               ,password updation and many more.
+  
+*/
 route.post('/user/add');
 route.post('/user/auth');
 route.get('/user/detail');
 route.get('/user/all');
 route.get('/user/password/forgot');
 route.post('/user/password/reset');
+
+/*
+  @author   :- Rohan Ravindra Kadam
+  @Purpose  :- Meals routes for add, updating and getting. 
+*/
+route.post('/meal/add');
+route.put('/meal/edit');
+route.get('/meal/remove');
+route.get('/meal/all')
+
+
+module.exports=route;
--- calories-backend/server.js
+++ calories-backend/server.js
@@ -1,5 +1,11 @@
 const express=require('express');
 const app=express();
+const calorieRoutes=require('./routes/calories.routes')
+
+
+//Adding Routes to server.js 
+app.use('/calories',calorieRoutes);
+
 app.listen('3000',()=>{
     console.log('Server Started at PORT 3000');
 })
