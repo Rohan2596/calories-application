f136a4823ee26c9175949cda10d8afab00125cd3
Rohan2596 <rohankadam662@gmail.com>
2020-11-16 18:46:46 +0530

    REFRACTED:App.js for better usbility.

--- calories-frontend/src/App.js
+++ calories-frontend/src/App.js
@@ -11,19 +11,16 @@ class App extends React.Component {
     }
   }
 changeState(){
-  const {isLogginActive} =this.state;
 
 }
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
+  
 
     return (
       <div className="App">
         <div className="login">
           <div className="container" >
-            {isLogginActive && <Register containerRef={(ref) => this.current = ref} />}
+            <Register  />
           </div>
          
         </div>
