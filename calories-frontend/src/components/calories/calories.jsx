3dd0809abd06382f1debd703b6c2f4f97e6d347d
Rohan2596 <rohankadam662@gmail.com>
2020-11-18 20:11:46 +0530

    ADDED:Dialog box for editmeal for same.

--- calories-frontend/src/components/calories/calories.jsx
+++ calories-frontend/src/components/calories/calories.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import '/home/admin1/Desktop/BackEndProjects/calories-application/calories-frontend/src/components/style.scss'
 import MealService from '../../services/meal.service';
+import Dialog from '@material-ui/core/Dialog';
+
 export class Calories extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             mealtitle: '',
             calCount: '',
-            meals: []
+            meals: [],
+            open: false,
+            setOpen: false
         }
     }
+    handleClickOpen = (item) => {
+        this.setState({
+            setOpen: true,
+            open: true,
+            mealtitle: item.title,
+            calCount: item.caloriesCount,
+        })
 
+    };
+    handleClose = () => {
+        this.setState({
+            setOpen: false
+        })
+    };
     onChangeMealtitle(event) {
         this.setState({
             mealtitle: event.target.value
@@ -25,7 +42,7 @@ export class Calories extends React.Component {
     componentDidMount() {
         this.getAllUserMeal();
     }
-    getAllUserMeal(){
+    getAllUserMeal() {
         MealService.getAllUserMeal().then((data) => {
             console.log(data);
             this.setState({ meals: data.data.data.meals })
@@ -80,14 +97,35 @@ export class Calories extends React.Component {
                     </div>
                     <div className="rightDiv">
                         <div className="labHeaderDiv">
-                            <div className="tableHeaderTextEdit" onClick={() => this.editMeal(item._id)}>Edit</div>
+                            <div className="tableHeaderTextEdit" onClick={() => this.handleClickOpen(item)}>Edit</div>
                         </div>
                         <div className='tableHeaderTextDelete' onClick={() => this.deleteMeal(item._id)} >
                             Delete
                         </div>
                     </div>
+                    <Dialog open={this.state.open} onClose={this.handleClose}>
+                        <div className="base-container" >
+                        <div className="content">
 
+                            <div className="form">
+                                <div className="form-group">
+                                    <label htmlFor="mealtitle">Meal Title</label>
+                                    <input type="text" name="title" placeholder="Meal Title" value={this.state.mealtitle} onChange={(event) => this.onChangeMealtitle(event)} />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Calories Count</label>
+                                    <input type="number" name="count" placeholder="Calories Count" value={this.state.calCount} onChange={(event) => this.onChangeCalCount(event)} />
+                                </div>
+
+                                <button type="button" className="btn" onClick={this.editMeal.bind(this)}>
+                                    editMeal
+</button>
+                            </div>
+                        </div>
+                        </div>
+                    </Dialog>
                 </div>
+
 
             );
         })
